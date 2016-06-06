4a9b9a4e8d4592b3267d26a8d5c2e22a4f2dd4d1
owebme <owebme@gmail.com>
2016-06-06 18:59:00 +0300

    ver 0.1

--- public/js/init.js
+++ public/js/init.js
@@ -6,7 +6,20 @@ yuapApp.reviews.init();
 
 window.onload = function(){
 
+    var $slider = $(".WD__products__container");
 
+    $slider.itemslide();
+
+    $(".WD__products__arrow__next").on("click", function(){
+        $slider.next();
+        $slider.reload();
+    });
+
+    $slider.on('changeActiveIndex', function(e) {
+        var index = $slider.getActiveIndex();
+        //if (index > 1)
+        console.log("new index: " + index);
+    });
 
 
 };
